3b559035791b5434d97b223bfee2b038ebf741d5
RichTee <RichTee@users.noreply.github.com>
2014-05-04 21:56:45 +0200

    Various Fireland updates
    Fireland bosses, need to change shannox + instance_firelands.cpp to make
    it more compatible.

--- src/server/scripts/Kalimdor/Firelands/firelands.h
+++ src/server/scripts/Kalimdor/Firelands/firelands.h
@@ -25,7 +25,7 @@
 
 uint32 const EncounterCount = 7;
 
-enum DataTypes
+enum Datas
 {
     DATA_BETH_TILAC         = 0,
     DATA_LORD_RHYOLITH      = 1,
@@ -36,14 +36,64 @@ enum DataTypes
     DATA_RAGNAROS           = 6,
 };
 
+enum GameObjectIds
+{
+	// Bethilac
+	GO_DOOR_BETHILAC			= 208877,
+
+	// Ragnaros
+	GO_RAGNAROS_DOOR            = 209073,
+    GO_RAGNAROS_PLATFORM        = 208835,
+    GO_CACHE_OF_THE_FIRELORD    = 209261,
+    GO_CACHE_OF_THE_FIRELORD_HC = 208967,
+};
+
 enum CreatureIds
 {
+	// Ragnaros
+	BOSS_RAGNAROS                   = 52409,
+    NPC_SULFURAS_SMASH_TRIGGER      = 53266,
+    NPC_SULFURAS_SMASH_TARGET       = 53268,
+    NPC_LAVA_WAVE                   = 53363,
+ 
+    NPC_ENGULFING_FLAMES_TRIGGER    = 53485,
+    NPC_SPLITTING_BLOW_TRIGGER      = 53393,
+    NPC_MAGMA_POOL_TRIGGER          = 53729,
+    NPC_PLATFORM_TRIGGER            = 53952,
+    NPC_MAGMA_TRAP                  = 53086,
+    NPC_SULFURAS_HAND_OF_RAGNAROS   = 53420,
+    NPC_SON_OF_FLAME                = 53140,
+    NPC_MOLTEN_ERUPTER              = 53617,
+    NPC_MOLTEN_SPEWER               = 53545,
+
+	// Alysrazor
     NPC_BLAZING_MONSTROSITY_LEFT    = 53786,
     NPC_BLAZING_MONSTROSITY_RIGHT   = 53791,
     NPC_EGG_PILE                    = 53795,
     NPC_HARBINGER_OF_FLAME          = 53793,
     NPC_MOLTEN_EGG_TRASH            = 53914,
     NPC_SMOULDERING_HATCHLING       = 53794,
+
+	// Shannox
+	NPC_RIPLIMB						= 53694,    // Shannox Dogs
+	NPC_RAGEFACE					 = 53695,
+	NPC_SHANNOX_SPEAR				 = 53752,    // Shannox Spear
+	NPC_CRYSTAL_TRAP        = 53713,
+	NPC_CRYSTAL_PRISON      = 53819,
+
+	// Bethilac
+	// Bethilac
+	NPC_CINDERWEB_SPINNER   = 53642,
+	NPC_CINDERWEB_DRONE     = 53635,
+	NPC_CINDERWEB_SPIDERLING = 53631,
+	NPC_ENGORGED_BROODLING  = 53745,
+	NPC_SPIDERWEB_FILAMENT  = 53082, 
+};
+
+enum Actions
+{
+	//Shannox
+	ACTION_SAY_RIPLIMB,		     //Shannox yell when Riplimb bites someone
 };
 
 class DelayedAttackStartEvent : public BasicEvent
